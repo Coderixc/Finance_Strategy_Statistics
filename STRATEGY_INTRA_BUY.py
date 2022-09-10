ba780585534f426c1f45812e8548ef3989c81db8
kamal chanchal <kchanchal78@gmail.com>
2022-09-10 23:32:43 +0530

    Added INTRADAY_NSE_EQ_OHLCV_TV.py 1.It Include all Intraday Data  in format like O H L C V TV 2.Impented coding, to fetch data from MYSQL with some custom built Function 3.Created First Strategy where Will Load Distinct Symbol before making and Conclusion #3

--- STRATEGY_INTRA_BUY.py
+++ STRATEGY_INTRA_BUY.py
@@ -1,11 +1,9 @@
 # 1. Take Intraday Data  for specific symbol or Universal  which is allowed.
-
-
-
-
-
+import ConfigVariable
 import INTRADAY_NSE_EQ_OHLCV_TV as OHLCV
 import pandas as pd
+
+from ConfigVariable import BhavCopy_EQ as ConfigVar
 
 #
 # obj = OHLCV.Data_Intraday_EQ()
@@ -17,37 +15,54 @@ class STRATEGY_INTRA_BUY:
 
     def __init__(self,df):
         self.df_OHLCV_Local=df
-        self.df_distinct_Symbol = pd.DataFrame()
+        self.df_distinct_Symbol=pd.DataFrame()
 
     def Distinct_EQ_Symbol(self):
+        self.df_distinct_Symbol=self.df_OHLCV_Local["Symbol"].unique()
+        return list(self.df_distinct_Symbol)
 
-        self.df_distinct_Symbol = self.df_OHLCV_Local["Symbol"].unique()
-        return  list(self.df_distinct_Symbol)
+    def Scan_Uptrend(self, df_input ,period):
+        """ Copy Data in Dataframe """
+        df_temp =df_input
+        df_temp_2 = pd.DataFrame()
 
+        for OnedayData in (df_temp.iterrows()):
+            sym = OnedayData[ConfigVar.Symbol] + "_"+OnedayData[ConfigVar.TIMESTAMP]
+            closeprice =OnedayData[ConfigVar.CLOSE]
+            print(sym,closeprice)
 
-    def Scan_Uptrend(self,period):
-        print("pass")
-
+        # print("pass")
 
 
 
 if __name__ == '__main__':
 
-    obj = OHLCV.Data_Intraday_EQ()
+    obj=OHLCV.Data_Intraday_EQ()
 
     obj.Intrady_Data_Universal()
 
-
-    d = pd.DataFrame()
-    df_OHLC_global= obj.df_INTRADAY_NSE_EQ_OHLC_TV
-    if d.count !=0:
-
+    d=pd.DataFrame()
+    df_OHLC_global=obj.df_INTRADAY_NSE_EQ_OHLC_TV
+    if d.count != 0:
         SIB=STRATEGY_INTRA_BUY(df_OHLC_global)
-        d =SIB.Distinct_EQ_Symbol()
-        print(d)
+        d=SIB.Distinct_EQ_Symbol()
+        df_1 = pd.DataFrame()
+
+        for symbol in d :
+            print ( "Scanning Symbol in df " + symbol )
+            """ Extract data of specific Symbol passed  """
+            filter_1_Search_Symbol = df_OHLC_global [ ConfigVariable.BhavCopy_EQ.Symbol ] == symbol
+            df_1 = df_OHLC_global.where ( filter_1_Search_Symbol , inplace = False )
+
+            if df_1.count != 0:
+                """ Data is Ready"""
+                df_1.dropna ( inplace = True )
+                SIB.Scan_Uptrend(df_1,5)
+
+                print(1)
+
+            else:
+                print("No Data is Present for Symbol " + symbol)
 
 
-
-
-
-
+#
