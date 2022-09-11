8971c9dec29a8377e3b195ef97b334893a616845
kamal chanchal <kchanchal78@gmail.com>
2022-09-11 17:24:00 +0530

    3.Calculate Column "Check_trend_XX" , XX  belongs  {"UP","DOWN"}     Func: It will check if , "If Trend(argument passed) matches with values with Column "UP_DOWN "             It this Matches , it will  insert "XX_P" where P denotes Passed,             if it doesn't match with  the value present in "UP_DOWN", it will insert XX_F, where F denotes passed calculation Purpose , We have ignored the First row and Marked With char "-".First row don't want to take participation in above three Classification

--- STRATEGY_INTRA_BUY.py
+++ STRATEGY_INTRA_BUY.py
@@ -1,4 +1,24 @@
-# 1. Take Intraday Data  for specific symbol or Universal  which is allowed.
+"""
+
+
+
+1. Take Intraday Data  for specific symbol or Universal  which is allowed.
+2. Created 2 Coloum "UP_DOWN","CHANGE_%",
+    UP_DOWN : compare prev Closing Price With Current Closing  and marked "UP" ,"DOWN","Consts"
+    CHANGE_% : Calculate Percentage Change With Previus Closing price with Current Closing
+
+    this Above calculation is Triggered by Function  Scan_Marking_UP_DOWN_CONST()
+
+
+3.Calculate Coloumn "Check_trend_XX" , XX  belongs  {"UP","DOWN"}
+    Func: It will check if , "If Trend(argument passed) matches with values with Column "UP_DOWN "
+            It this Matches , it will  insert "XX_P" where P denotes Passed,
+            if it doesn't matches with with the value present in "UP_DOWN", it will insert XX_F, wher F denotes passed
+
+
+"""
+
+
 import ConfigVariable
 import INTRADAY_NSE_EQ_OHLCV_TV as OHLCV
 import pandas as pd
@@ -21,38 +41,83 @@ class STRATEGY_INTRA_BUY:
         self.df_distinct_Symbol=self.df_OHLCV_Local["Symbol"].unique()
         return list(self.df_distinct_Symbol)
 
-    def Scan_Uptrend(self, df_input ,period = 50):
+    def Calculate_Percentage ( self , input1 , input2_base ) :
+        try :
+            res = (input1 - input2_base) / input2_base * 100
+            return round(res,2)
+        except :
+            res = 0.0
+            return res
+
+    def Scan_Marking_UP_DOWN_CONST( self , df_input , period = 50 ) :
         """ Copy Data in Dataframe """
-        df_temp =df_input[::-1].iloc[0:period][::-1]
+        df_temp = df_input[ : :-1 ].iloc[ 0 :period ][ : :-1 ]
         closingPrice_Prev = 0.0
-        closingPrice_Current =0.0
+        closingPrice_Current = 0.0
 
-        list_storeAction =[]
+        list_storeAction = [ ]
+        list_percentage = [ ]
+        try :
+            for OnedayData in (df_temp.iterrows( )) :
 
-        for OnedayData in (df_temp.iterrows( )) :
-            # sym = str(OnedayData [ ConfigVar.Symbol ]) + "_" + str(OnedayData [ ConfigVar.TIMESTAMP ])
-            closeprice = float(OnedayData[1] [ ConfigVar.CLOSE ])
+                # sym = str(OnedayData [ ConfigVar.Symbol ]) + "_" + str(OnedayData [ ConfigVar.TIMESTAMP ])
+                closeprice = float( OnedayData[ 1 ][ ConfigVar.CLOSE ] )
 
-            closingPrice_Current = closeprice
+                closingPrice_Current = closeprice
 
-            if closingPrice_Prev != 0:
-                if closingPrice_Current > closingPrice_Prev:
-                    list_storeAction.append ( "UP" )
-                elif closingPrice_Current < closingPrice_Prev:
-                    list_storeAction.append ( "DOWN" )
+                if closingPrice_Prev != 0 :
+                    if closingPrice_Current > closingPrice_Prev :
+                        list_storeAction.append( "UP" )
+                        list_percentage.append( self.Calculate_Percentage( closingPrice_Current , closingPrice_Prev ) )
+
+                    elif closingPrice_Current < closingPrice_Prev :
+                        list_storeAction.append( "DOWN" )
+                        list_percentage.append( self.Calculate_Percentage( closingPrice_Current , closingPrice_Prev ) )
+
+                    else :
+                        list_storeAction.append( "Const" )
+                        list_percentage.append( self.Calculate_Percentage( closingPrice_Current , closingPrice_Prev ) )
+
+                else :
+                    list_storeAction.append( "-" )
+                    list_percentage.append( "0" )
+
+                # Update Previous Closing Price Variable
+                closingPrice_Prev = closeprice
+
+                # print ( sym , closeprice )
+            df_temp[ "UP_DOWN" ] = list_storeAction
+            df_temp[ "CHNAGE_%" ] = list_percentage
+        except :
+            print( "Failed to Process function :" )
+
+        finally :
+            return df_temp
+
+
+    def Apply_Trend_On_Period( self,df_Input_With_Col_UP_DOWN_CHNAGE,trend, period=5 ):
+
+
+        try:
+            dt_temp = df_Input_With_Col_UP_DOWN_CHNAGE[ : :-1 ].iloc[ 0 :period ][::-1]
+
+            list_Result =[]
+
+            for row in dt_temp.iterrows():
+                updown = row[1]["UP_DOWN" ]
+                if(updown == trend):
+                    list_Result.append(trend +"_P")
                 else:
-                    list_storeAction.append ( "Const" )
-            else:
-                list_storeAction.append ( "-" )
+                    list_Result.append(trend +"_F")
 
-            # Update Previous Closing Price Variable
-            closingPrice_Prev = closeprice
+            dt_temp["Check_Trend_"+trend] = list_Result
 
-            # print ( sym , closeprice )
-        df_temp["UP_DOWN"] =list_storeAction
-        print("pass")
+            return dt_temp
 
+        except:
+            print("Failed to Apply Trend on Apply_Trend_On_Period()")
 
+            return pd.DataFrame()
 
 
 
@@ -78,9 +143,13 @@ if __name__ == '__main__':
             if df_1.count != 0:
                 """ Data is Ready"""
                 df_1.dropna ( inplace = True )
-                SIB.Scan_Uptrend(df_1)
+                df_marked_U_D_C=  SIB.Scan_Marking_UP_DOWN_CONST(df_1,200)
 
-                print(1)
+                """Calculate Trend  on specific Period """
+                df_Apply_trend = SIB.Apply_Trend_On_Period(df_marked_U_D_C,"UP",10)
+
+
+
 
             else:
                 print("No Data is Present for Symbol " + symbol)
